04ccdb18a6c11f508d3e7f6fade26398d56f2c35
Vaidehi Jadhav <jadhav.v10122@gmail.com>
2024-06-11 21:02:06 +0530

    study different list methods

---
+++ listMethod.py
@@ -0,0 +1,25 @@
+l = [11, 54, 9,65,22,100,445,6,4,20, 9]
+print(l)
+# l.sort()  #sort in ascending order
+# l.sort(reverse=True)  #sort in descending order
+
+# l.reverse() #reverse order of string
+
+# print(l.index(9))  #returns the index of the first occurrence of the list item
+
+# print(l.count(11))  #return the count of occurrence of item in list
+
+# m = l  #change in original list
+# m = l.copy()  #Returns copy of the list. This can be done to perform operations on the list without modifying the original list.
+# m[0] = 0 
+
+# l.append(10)  #appends items to the end
+
+# l.insert(0, 2)  #User has to specify index and the item to be inserted within the insert() method.
+
+# m = [22, 23, 19]
+# l.extend(m) #adds an entire list or any other collection datatype 
+# k = l + m   #concatenate two lists to join two lists
+# print(k)
+
+print(l)
