98a64a4d59ddd0279f7539e3c1ad9c80df6632d1
Vaidehi Jadhav <jadhav.v10122@gmail.com>
2024-06-19 20:50:18 +0530

    add tuple operation

--- tuple.py
+++ tuple.py
@@ -1,11 +1,33 @@
 # tup = (1)
 # print(type(tup))     #output list
 
-tup = (1,)
-print(type(tup))   #output tuple
+# tup = (1,)
+# print(type(tup))   #output tuple
 
-country = ("Spain", "Italy", "India", "England", "Germany")
-if "Germany" in country:
-    print("Germany is present.")
-else:
-    print("Germany is absent.")
+# country = ("Spain", "Italy", "India", "England", "Germany")
+# if "Germany" in country:
+#     print("Germany is present.")
+# else:
+#     print("Germany is absent.")
+
+
+## Tuple operations/Methods
+# Manipulating Tuple
+
+# fruits = ("apples", "banana", "mango", "guava", "grapes")
+# temp = list(fruits)
+# temp.append("orange")
+# temp.pop(1)
+# fruits = tuple(temp)
+# print(fruits)
+
+#concatenation of tuples
+# fruits = ("apples", "banana", "mango", "guava", "grapes")
+# flowers = ("rose", "lily", "lotus","perwinkle")
+# print(fruits + flowers)
+
+#count
+Tuple1 = (0, 1, 2, 3, 2, 3, 1, 3, 2)
+# res = Tuple1.count(3)
+
+print('Count of 3 in Tuple1 is:', res)
