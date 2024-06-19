551175dcf0faae04e85dc1adc9662c1fd15c02ff
Vaidehi Jadhav <jadhav.v10122@gmail.com>
2024-06-19 20:17:21 +0530

    create tuple in python

---
+++ tuple.py
@@ -0,0 +1,11 @@
+# tup = (1)
+# print(type(tup))     #output list
+
+tup = (1,)
+print(type(tup))   #output tuple
+
+country = ("Spain", "Italy", "India", "England", "Germany")
+if "Germany" in country:
+    print("Germany is present.")
+else:
+    print("Germany is absent.")
