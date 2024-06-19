8af9c07ed17ce4f1f34afb99da9a31fbe92aa735
Vaidehi Jadhav <jadhav.v10122@gmail.com>
2024-06-19 20:51:13 +0530

    Change condition add and

--- Excercise 2/excercise2.py
+++ Excercise 2/excercise2.py
@@ -5,11 +5,11 @@ print(timestamp)
 h = int(time.strftime("%H"))
 n = input("Enter Your name: ")
 
-if(4>=h<12):
+if(4>=h and h<12):
     print("Good morning,", n.capitalize(), "its", timestamp)
-elif(12>=h<=16):
+elif(12>=h and h<=16):
     print("Good Afternoon,", n.capitalize(), "its", timestamp)
-elif(17>=h<=19):
+elif(17>=h and h<=19):
     print("Good Evening,", n.capitalize(), "its", timestamp)
 else:
     print("Good night,", n.capitalize(),"its", timestamp)
