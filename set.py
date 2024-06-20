df42f5028ebf3a7dcb6b1dd95d5740b0372992be
Vaidehi Jadhav <jadhav.v10122@gmail.com>
2024-06-20 21:08:31 +0530

    sets in python

---
+++ set.py
@@ -0,0 +1,12 @@
+# s = {2, 4, 6, 4}
+# print(s)
+
+# ray = {}   #dic
+# ray = set()  #empty set
+# print(type(ray))
+
+info = {"vaidehi", 22 , 10.3, "India"}
+# print(info)
+
+for value in info:
+    print(value)
