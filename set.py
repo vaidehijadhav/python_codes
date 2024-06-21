57fc9a11ca0622c6187c34889f91cd0d5b01765c
Vaidehi Jadhav <jadhav.v10122@gmail.com>
2024-06-21 22:49:39 +0530

    set and joining of set

--- set.py
+++ set.py
@@ -5,8 +5,31 @@
 # ray = set()  #empty set
 # print(type(ray))
 
-info = {"vaidehi", 22 , 10.3, "India"}
-# print(info)
+# info = {"vaidehi", 22 , 10.3, "India"}
+# # print(info)
 
-for value in info:
-    print(value)
+# for value in info:
+#     print(value)
+
+# Joining of set
+set1 = {1,2,3,4}
+set2 = {2,4,6,8}
+
+## union() and update()
+# set3 = set1.union(set2)
+# set1.update(set2)
+
+## intersection() and intersection_update()
+# set3 = set1.intersection(set2)
+# set1.intersection_update(set2)
+
+##symmetric_difference and symmetric_difference_update()
+# set3 = set1.symmetric_difference(set2)
+# set1.symmetric_difference_update(set2)
+
+
+## difference() and difference_update()
+set3 =set1.difference(set2)
+set1.difference_update(set2)
+print(set1)
+print(set3)
