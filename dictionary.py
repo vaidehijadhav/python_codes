466dda83c2043da289d08cb2e465d26e20a43d2d
Vaidehi Jadhav <jadhav.v10122@gmail.com>
2024-06-22 21:22:50 +0530

    dictionary

---
+++ dictionary.py
@@ -0,0 +1,15 @@
+info = {'name': 'vaidehi', 'age':22, 'eligible':True}
+# print(info)
+
+# I. Accessing single values:
+# print(info['name'])
+# print(info.get('eligible'))
+
+# II. Accessing multiple values:
+# print(info.values())
+
+# III. Accessing keys:
+# print(info.keys())
+
+# IV. Accessing key-value pairs:
+print(info.items())
