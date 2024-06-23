567e1dabde7deaf071be0de79b8f2517c7e0d8a6
Vaidehi Jadhav <jadhav.v10122@gmail.com>
2024-06-23 12:57:49 +0530

    else statement with loop

---
+++ loopWithElse.py
@@ -0,0 +1,22 @@
+# for i in range(5):
+#     print(i)
+
+# else:
+#     print("The loop is over")
+
+# for i in range(7):
+#     print(i)
+#     if i == 4:
+#         break
+
+# else:
+#     print("The loop is over")           #else part doesn't execute as loop didn't complete
+
+
+i = 0
+while i < 6:
+    print(i)
+    i = i+1
+
+else:
+    print("The loop is over")             
