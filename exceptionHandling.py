3b9599e6c009b4cb0d9a4e822974456c9a3864b2
Vaidehi Jadhav <jadhav.v10122@gmail.com>
2024-06-23 13:24:45 +0530

    Exception Handling

---
+++ exceptionHandling.py
@@ -0,0 +1,9 @@
+a = input("Enter the number: ")
+print(f"Multiplication table of {a} is: ")
+try:
+    for i in range(1, 11):
+        print(f"{int(a)} X {i} = {int(a)*i}")
+except:
+    print("Invalid Input!")
+
+print("above program is completed")
