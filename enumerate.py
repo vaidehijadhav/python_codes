c4a4d06319594daa8337ac226b7e19ce7584da82
Vaidehi Jadhav <jadhav.v10122@gmail.com>
2024-06-24 13:24:18 +0530

    Enumerate Function

---
+++ enumerate.py
@@ -0,0 +1,13 @@
+marks = [29, 49,54,32,56,77,65,99]
+
+# index = 0
+# for mark in marks:
+#     print(mark)
+#     if(index == 3):
+#         print("marks")
+#     index +=1
+    
+for index, mark in enumerate(marks):
+    print(mark)
+    if(index == 3):
+        print("marks")
