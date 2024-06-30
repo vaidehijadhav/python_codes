49f2af2909a8b5ba1495adb99d380f6627fc418c
Vaidehi Jadhav <jadhav.v10122@gmail.com>
2024-06-30 23:56:42 +0530

    readline and writeline

--- FileHandling/main.py
+++ FileHandling/main.py
@@ -5,6 +5,29 @@
 # print(text)
 # f.close()
 
-with open("FileHandling/myfile.txt", 'a') as f:
-    f.write("Let's get started!!")
+# with open("FileHandling/myfile.txt", 'a') as f:
+#     f.write("Let's get started!!")
 
+
+# f = open("FileHandling/myfile.txt", 'r')
+# i = 0
+# while True:
+#     i = i + 1
+#     line = f.readline()
+#     if not line:
+#         break
+
+#     m1 = int(line.split(",")[0])
+#     m2 = int(line.split(",")[1])
+#     m3 = int(line.split(",")[2])
+
+#     print(f"Marks of studnet {i} in Maths is: {m1}")
+#     print(f"Marks of studnet {i} in Maths is: {m2}")
+#     print(f"Marks of studnet {i} in Maths is: {m3}")
+# print(line)
+
+
+f = open('FileHandling/myfile2.txt', 'w')
+lines = ['line 1\n', 'line 2\n', 'line 3\n']
+f.writelines(lines)
+f.close()
